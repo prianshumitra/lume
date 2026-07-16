21cf8c5b09cf6d2d8bc3cfe8960daf02da7936a4
Prianshu Mitra <prianshumitraprivateserver1@gmail.com>
2026-07-17 03:29:25 +0530

    creating user database for authorization

--- app/main.py
+++ app/main.py
@@ -209,3 +209,12 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 #
 #------------------------------------------------------------------------------------------------------------------------------
 
+# =-CREATE USER-=
+@app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import datetime
 
 # post to be used in get request to get data from database
@@ -10,11 +10,24 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
+#post response model
 class Post(PostBase): #response model schema
     id: int
     created_at: datetime
 
     # to use orm_mode in models.py
     #mandatory for response model
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+#user response model
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
     class Config:
         orm_mode = True
