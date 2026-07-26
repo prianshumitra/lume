c0e926d2d067f6261bcfce323f9f75ddb5d26149
Prianshu Mitra <prianshumitraprivateserver1@gmail.com>
2026-07-26 11:53:25 +0530

    integrated CORS middleware to allow request from any origin

--- app/main.py
+++ app/main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post,user,auth,vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 #models.Base.metadata.create_all(bind=engine) #to create tables in database when no migration tool present(alembic)
 
 app = FastAPI()
 #app->instance
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #routers
 app.include_router(post.router)
